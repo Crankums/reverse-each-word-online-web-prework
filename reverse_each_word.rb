48962e9d03ba5dc10d4fb25df14ac69972f3c65e
Craig Donavin <crdonavin@gmail.com>
2019-03-24 21:43:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(string)
-rev_arr = []
-new_str = string.reverse
-new_arr = new_str.split
-new_arr.each {|i| rev_arr.unshift(i)}
-rev_arr.join(" ")
+  rev_arr = []
+  new_str = string.reverse
+  new_arr = new_str.split
+  new_arr.each {|i| rev_arr.unshift(i)}
+  rev_arr.join(" ")
 end
 
 def
