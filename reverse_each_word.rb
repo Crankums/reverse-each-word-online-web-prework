72423877d4e109aa029be909124656ae5df12b87
Craig Donavin <crdonavin@gmail.com>
2019-03-24 21:38:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
-new_arr = []
+rev_arr = []
 new_str = string.reverse
-new_str.split
-new_str.each {|i| new_arr.unshift(i)}
+new_arr = new_str.split
+new_arr.each {|i| new_arr.unshift(i)}
 end
